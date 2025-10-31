0f7f1e5c97aa9e8335b14c4b8cf8f85ced2fd6f7
PumpkinDude4242 <an.allouache@gmail.com>
2025-10-31 21:08:10 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Silicium360Site from "../site_vitrine_silicium_360.jsx";
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<Silicium360Site />);
-)
+// ✅ importe ton site vitrine (depuis la racine du projet)
+import Silicium360Site from "../site_vitrine_silicium_360.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Silicium360Site />
+  </React.StrictMode>
+);
