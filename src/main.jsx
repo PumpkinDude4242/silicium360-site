d573f969bab0ba8a6d8f51849b7ec9281d88c72d
PumpkinDude4242 <an.allouache@gmail.com>
2025-11-26 16:48:49 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Silicium360Site from "./site_vitrine_silicium_360.jsx";
+import Silicium360Site from "../site_vitrine_silicium_360.jsx";
 import MentionsLegalesPage from "./pages/MentionsLegales.jsx";
 import PolitiqueConfidentialitePage from "./pages/PolitiqueConfidentialite.jsx";
 
